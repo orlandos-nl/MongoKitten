47a3956c4108d6de1c8abeea37cf53b414a4816f
Robbert Brandsma <i@robbertbrandsma.nl>
2016-03-07 12:23:49 +0100

    Specify the right version of BlueSocket

--- Package.swift
+++ Package.swift
@@ -5,6 +5,6 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/PlanTeam/When.git", majorVersion: 1),
         .Package(url: "https://github.com/PlanTeam/BSON.git", majorVersion: 1),
-        .Package(url: "https://github.com/IBM-Swift/BlueSocket.git", majorVersion: 0)
+        .Package(url: "https://github.com/IBM-Swift/BlueSocket.git", Version(0,0,4))
     ]
 )
