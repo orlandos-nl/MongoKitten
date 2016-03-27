819cd875edb951b8a2e26054d40838a3ed7183d3
Joannis Orlandos <joannis@orlandos.nl>
2016-03-27 22:49:09 +0200

    fixed package.swift

--- Package.swift
+++ Package.swift
@@ -6,6 +6,7 @@ let package = Package(
         .Package(url: "https://github.com/PlanTeam/BSON.git", majorVersion: 1, minor: 2),
         .Package(url: "https://github.com/SwiftX/C7.git", majorVersion: 0, minor: 1),
         .Package(url: "https://github.com/ketzusaka/Hummingbird", majorVersion: 1, minor: 1),
+        .Package(url: "https://github.com/johnno1962/NSLinux.git", majorVersion: 1, minor: 1),
     ]
 )
 
