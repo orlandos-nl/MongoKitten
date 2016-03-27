4cd89ad52574510bcfda50308282ccd746e55857
Logan Wright <logan.william.wright@gmail.com>
2016-03-27 19:16:53 -0400

    fixing name issue

--- Package.swift
+++ Package.swift
@@ -3,9 +3,9 @@ import PackageDescription
 let package = Package(
     name: "MongoKitten",
     dependencies: [
-        .Package(url: "https://github.com/PlanTeam/BSON.git", majorVersion: 1, minor: 2),
-        .Package(url: "https://github.com/SwiftX/C7.git", majorVersion: 0, minor: 1),
-        .Package(url: "https://github.com/ketzusaka/Hummingbird.git", majorVersion: 1, minor: 1),
+        .Package(url: "https://github.com/PlanTeam/BSON", majorVersion: 1, minor: 2),
+        .Package(url: "https://github.com/SwiftX/C7", majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/ketzusaka/Hummingbird", majorVersion: 1, minor: 1),
     ]
 )
 
