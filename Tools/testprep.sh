655d1980dc1e3f9051b10a44c43aa238d95f05ce
Robbert Brandsma <i@robbertbrandsma.nl>
2016-04-19 16:01:42 +0200

    Updated testprep.sh

--- Tools/testprep.sh
+++ Tools/testprep.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+echo "MongoKitten Unit Test Preparation Script"
+
+unamestr=`uname`
+if [[ "$unametr" == 'Darwin' ]]; then
+	brew update
+	brew install mongodb
+fi
+
+mongodbpath="/tmp/mongokitten-unittest-db"
+mkdir -p $mongodbpath
+mongod --fork --syslog --dbpath $mongodbpath
+
 mongoimport --db=mongokitten-unittest --collection=zips --drop zips.json
