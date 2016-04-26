a6a04cfb1f964fd5c53d786723dea8b0531d9bbf
Joannis Orlandos <joannis@orlandos.nl>
2016-04-26 14:58:36 +0200

    Using proper BSON version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     dependencies: [
                       .Package(url: "https://github.com/CryptoKitten/MD5.git", majorVersion: 0, minor: 2),
                       .Package(url: "https://github.com/CryptoKitten/SCRAM.git", majorVersion: 0, minor: 4),
-                      .Package(url: "https://github.com/PlanTeam/BSON.git", majorVersion: 0, minor: 0),
+                      .Package(url: "https://github.com/PlanTeam/BSON.git", majorVersion: 2, minor: 1),
                       .Package(url: "https://github.com/CryptoKitten/SHA1.git", majorVersion: 0, minor: 4),
                       .Package(url: "https://github.com/ketzusaka/Strand.git", majorVersion: 1, minor: 2),
                       ]
