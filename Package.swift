508a220f51f5b40467174789ea2897e95da3d93b
Joannis Orlandos <joannis@orlandos.nl>
2016-04-27 11:31:30 +0200

    Updated package file for 04-25 Linux

--- Package.swift
+++ Package.swift
@@ -3,10 +3,10 @@ import PackageDescription
 let package = Package(
     name: "MongoKitten",
     dependencies: [
-                      .Package(url: "https://github.com/CryptoKitten/MD5.git", majorVersion: 0, minor: 2),
-                      .Package(url: "https://github.com/CryptoKitten/SCRAM.git", majorVersion: 0, minor: 4),
+                      .Package(url: "https://github.com/CryptoKitten/MD5.git", majorVersion: 0, minor: 5),
+                      .Package(url: "https://github.com/CryptoKitten/SCRAM.git", majorVersion: 0, minor: 5),
                       .Package(url: "https://github.com/PlanTeam/BSON.git", majorVersion: 2, minor: 2),
-                      .Package(url: "https://github.com/CryptoKitten/SHA1.git", majorVersion: 0, minor: 4),
+                      .Package(url: "https://github.com/CryptoKitten/SHA1.git", majorVersion: 0, minor: 5),
                       .Package(url: "https://github.com/ketzusaka/Strand.git", majorVersion: 1, minor: 2),
                       ]
 )
