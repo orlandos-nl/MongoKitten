e271c7dcf81c560aa2a3dd957e30b1eac1a88a86
Joannis Orlandos <joannis@orlandos.nl>
2016-05-15 13:18:40 +0200

    Added GridFS docs

--- Sources/GridFS.swift
+++ Sources/GridFS.swift
@@ -11,13 +11,21 @@ import Foundation
 import MD5
 
 /// A GridFS instance similar to a collection
+///
+/// Conforms to the GridFS standard as specified here: https://docs.mongodb.org/manual/core/gridfs/
 public class GridFS {
+    /// The bucket for file data
     private let files: Collection
+    
+    /// The bucket for file data chunks
     private let chunks: Collection
     
-    /// Initializes a GridFS Collection (bucket) in a given database
+    /// Initializes a GridFS `Collection` (bucket) in a given database
+    ///
     /// - parameter in: In which database does this GridFS bucket reside
     /// - parameter named: The optional name of this GridFS bucket (by default "fs")
+    ///
+    /// - throws: When we can't send the request/receive the response, you don't have sufficient permissions or an error occurred or when it can't create it's indexes
     public init(in database: Database, named bucketName: String = "fs") throws {
         files = database["\(bucketName).files"]
         chunks = database["\(bucketName).chunks"]
@@ -29,51 +37,79 @@ public class GridFS {
     }
     
     /// Finds using all files matching this ObjectID
+    ///
     /// - parameter byID: The ID to look for
+    ///
+    /// - throws: When we can't send the request/receive the response, you don't have sufficient permissions or an error occurred
+    ///
     /// - returns: A cursor pointing to all resulting files
     public func find(byID id: ObjectId) throws -> Cursor<File> {
-        return try self.find(matching: ["_id": .objectId(id)])
+        return try self.find(matching: ["_id": .objectId(id)] as Document)
     }
     
     /// Finds using all files file matching this filename
+    ///
     /// - parameter filter: The filename to look for
+    ///
+    /// - throws: When we can't send the request/receive the response, you don't have sufficient permissions or an error occurred
+    ///
     /// - returns: A cursor pointing to all resulting files
     public func find(byName filename: String) throws -> Cursor<File> {
-        return try self.find(matching: ["filename": .string(filename)])
+        return try self.find(matching: ["filename": .string(filename)] as Document)
     }
     
     /// Finds using all files matching this MD5 hash
+    ///
     /// - parameter filter: The hash to look for
+    ///
+    /// - throws: When we can't send the request/receive the response, you don't have sufficient permissions or an error occurred
+    ///
     /// - returns: A cursor pointing to all resulting files
     public func find(byHash hash: String) throws -> Cursor<File> {
-        return try self.find(matching: ["md5": .string(hash)])
+        return try self.find(matching: ["md5": .string(hash)] as Document)
     }
     
     /// Finds the first file matching this ObjectID
+    ///
     /// - parameter byID: The hash to look for
+    ///
+    /// - throws: When we can't send the request/receive the response, you don't have sufficient permissions or an error occurred
+    ///
     /// - returns: The resulting file
     public func findOne(byID id: ObjectId) throws -> File? {
-        return try self.find(matching: ["_id": .objectId(id)]).makeIterator().next()
+        return try self.find(matching: ["_id": .objectId(id)] as Document).makeIterator().next()
     }
     
     /// Finds the first file matching this filename
+    ///
     /// - parameter byName: The filename to look for
+    ///
+    /// - throws: When we can't send the request/receive the response, you don't have sufficient permissions or an error occurred
+    ///
     /// - returns: The resulting file
     public func findOne(byName filename: String) throws -> File? {
-        return try self.find(matching: ["filename": .string(filename)]).makeIterator().next()
+        return try self.find(matching: ["filename": .string(filename)] as Document).makeIterator().next()
     }
     
     /// Finds the first file matching this MD5 hash
+    ///
     /// - parameter byHash: The hash to look for
+    ///
+    /// - throws: When we can't send the request/receive the response, you don't have sufficient permissions or an error occurred
+    ///
     /// - returns: The resulting file
     public func findOne(byHash hash: String) throws -> File? {
-        return try self.find(matching: ["md5": .string(hash)]).makeIterator().next()
+        return try self.find(matching: ["md5": .string(hash)] as Document).makeIterator().next()
     }
     
     /// Finds using a matching filter
+    ///
     /// - parameter filter: The filter to use
+    ///
+    /// - throws: When we can't send the request/receive the response, you don't have sufficient permissions or an error occurred
+    ///
     /// - returns: A cursor pointing to all resulting files
-    public func find(matching filter: Document) throws -> Cursor<File> {
+    public func find(matching filter: QueryProtocol) throws -> Cursor<File> {
         let cursor = try files.find(matching: filter)
         
         let gridFSCursor: Cursor<File> = Cursor(base: cursor, transform: { File(document: $0, chunksCollection: self.chunks, filesCollection: self.files) })
@@ -179,6 +215,10 @@ public class GridFS {
         let filesCollection: Collection
         
         /// Initializes from a file-collection Document
+        ///
+        /// - parameter document: The `File`'s `Document` that has been found in the files `Collection`
+        /// - parameter chunksCollection: The `Collection` where the `File` `Chunk`s are stored
+        /// - parameter chunksCollection: The `Collection` where the `File` data is stored
         internal init?(document: Document, chunksCollection: Collection, filesCollection: Collection) {
             guard let id = document["_id"].objectIdValue,
                 length = document["length"].int32Value,
@@ -214,6 +254,11 @@ public class GridFS {
         }
         
         /// Finds all or specific chunks
+        ///
+        /// Returns the bytes you selected
+        ///
+        /// - parameter start: The `Byte` where you start fetching
+        /// - parameter end: The `Byte` where you stop fetching
         public func read(from start: Int = 0, to end: Int? = nil) throws -> [Byte] {
             let remainderValue = start % Int(self.chunkSize)
             let skipChunks = (start - remainderValue) / Int(self.chunkSize)
@@ -230,7 +275,6 @@ public class GridFS {
             
             let lastByte = start + bytesRequested
             let lastChunkRemainder = lastByte % Int(self.chunkSize)
-//            let finalReduction = Int(self.chunkSize) - lastChunkRemainder
             
             var endChunk = (lastByte - lastChunkRemainder) / Int(self.chunkSize)
             
@@ -259,19 +303,19 @@ public class GridFS {
             /// The ID of the file that this chunk is a part of
             let filesID: ObjectId
             
-            /// Which chunk we are
+            /// Which chunk this is
             let n: Int32
             
             /// The data for our chunk
             let data: [Byte]
             
-            /// The chunk Collection which this chunk is in
+            /// The chunk `Collection` which this chunk is stored in
             let chunksCollection: Collection
             
-            /// The files collection where our file is stored
+            /// The files `Collection` where our file is stored
             let filesCollection: Collection
             
-            /// Initializes with a Document found when looking for chunks
+            /// Initializes with a `Document` found when looking for chunks
             init?(document: Document, chunksCollection: Collection, filesCollection: Collection) {
                 guard let id = document["_id"].objectIdValue,
                     filesID = document["files_id"].objectIdValue,
