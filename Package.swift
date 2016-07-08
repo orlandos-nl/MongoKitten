a303d8bcfb795b2daf4937d03ad8997e921290d5
Robbert Brandsma <i@robbertbrandsma.nl>
2016-07-08 10:11:59 +0200

    Upgraded dependencies and Swift version

--- Package.swift
+++ Package.swift
@@ -4,20 +4,20 @@ var package = Package(
     name: "MongoKitten",
     dependencies: [
         // For MongoCR authentication
-        .Package(url: "https://github.com/CryptoKitten/MD5.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/CryptoKitten/MD5.git", majorVersion: 0, minor: 8),
         
         // For SCRAM-SHA-1 authentication
-        .Package(url: "https://github.com/CryptoKitten/SCRAM.git", majorVersion: 0, minor: 7),
-        .Package(url: "https://github.com/CryptoKitten/SHA1.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/CryptoKitten/SCRAM.git", majorVersion: 0, minor: 8),
+        .Package(url: "https://github.com/CryptoKitten/SHA1.git", majorVersion: 0, minor: 8),
         
         // For MongoDB Documents
         .Package(url: "https://github.com/OpenKitten/BSON.git", Version(0,0,0)),
         
         // Provides sockets
-        .Package(url: "https://github.com/czechboy0/Socks.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/czechboy0/Socks.git", majorVersion: 0, minor: 8),
 
         // Background queue
-        .Package(url: "https://github.com/ketzusaka/Strand.git", majorVersion: 1, minor: 4),
+        .Package(url: "https://github.com/ketzusaka/Strand.git", majorVersion: 1, minor: 5),
 
         ]
 )
