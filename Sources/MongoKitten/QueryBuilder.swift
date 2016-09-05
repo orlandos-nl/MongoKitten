23e617384dfb37d75721b28a4f4b29c8aa626c5b
Paul Rolfe <paul.a.rolfe@gmail.com>
2016-09-05 13:27:21 -0400

    no need for unwrapping this string within a string. and 'm' will have no effect

--- Sources/MongoKitten/QueryBuilder.swift
+++ Sources/MongoKitten/QueryBuilder.swift
@@ -245,7 +245,7 @@ public indirect enum AQT {
         case .not(let aqt):
             return ["$not": ~aqt.document]
         case .contains(let key, let val):
-            return [key: .regularExpression(pattern: "\(val)", options: "m")]
+            return [key: .regularExpression(pattern: val, options: "")]
         case .startsWith(let key, let val):
             return [key: .regularExpression(pattern: "^\(val)", options: "m")]
         case .endsWith(let key, let val):
