efcc33a45b11339169fccc59f180514b2bd994e1
Joannis Orlandos <j.orlandos@jakajima.eu>
2016-10-10 15:10:19 +0200

    DBRef support

---
+++ Sources/MongoKitten/DBRef.swift
@@ -0,0 +1,62 @@
+import BSON
+
+public struct DBRef {
+    var collection: Collection
+    var id: Value
+    
+    public init(referencing reference: Value, inCollection collection: Collection) {
+        self.id = reference
+        self.collection = collection
+    }
+    
+    public init(referencing reference: ObjectId, inCollection collection: Collection) {
+        self.id = ~reference
+        self.collection = collection
+    }
+    
+    public init?(_ document: Document, inServer server: Server) {
+        guard let database = document["$db"].stringValue, let collection = document["$ref"].stringValue else {
+            return nil
+        }
+        
+        let id = document["$id"]
+        
+        guard id != .nothing else {
+            return nil
+        }
+        
+        self.collection = server[database][collection]
+        self.id = id
+    }
+    
+    public init?(_ document: Document, inDatabase database: Database) {
+        guard let collection = document["$ref"].stringValue else {
+            return nil
+        }
+        
+        let id = document["$id"]
+        
+        guard id != .nothing else {
+            return nil
+        }
+        
+        self.collection = database[collection]
+        self.id = id
+    }
+    
+    public var documentValue: Document {
+        return [
+            "$ref": ~self.collection.name,
+            "$id": self.id,
+            "$db": ~self.collection.database.name
+        ]
+    }
+    
+    public var bsonValue: Value {
+        return ~self.documentValue
+    }
+    
+    public func resolve() throws -> Document? {
+        return try collection.findOne(matching: "_id" == self.id)
+    }
+}
