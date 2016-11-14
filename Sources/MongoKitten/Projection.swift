d15f87241a84ccae5000b3a98c1d40809d87655f
Joannis Orlandos <joannis@orlandos.nl>
2016-11-14 14:22:51 +0100

    Aggregates

--- Sources/MongoKitten/Projection.swift
+++ Sources/MongoKitten/Projection.swift
@@ -3,8 +3,26 @@ import BSON
 public struct Projection {
     var document: Document
     
+    public enum Expression: ValueConvertible {
+        case custom(ValueConvertible)
+        case included
+        case excluded
+        
+        public func makeBsonValue() -> BSON.Value {
+            switch self {
+            case .custom(let convertible): return convertible.makeBsonValue()
+            case .included: return true
+            case .excluded: return false
+            }
+        }
+    }
+    
     public init(_ document: Document) {
         self.document = document
+    }
+    
+    public mutating func suppressIdentifier() {
+        document["_id"] = false
     }
 }
 
@@ -17,13 +35,16 @@ extension Projection: ValueConvertible {
 extension Projection: ExpressibleByArrayLiteral {
     public init(arrayLiteral elements: String...) {
         self.document = Document(dictionaryElements: elements.map {
-            return ($0, Int32(1))
+            return ($0, true)
         }).flattened()
     }
 }
 
 extension Projection: ExpressibleByDictionaryLiteral {
-    public init(dictionaryLiteral elements: (String, ValueConvertible)...) {
-        self.document = Document(dictionaryElements: elements).flattened()
+    public init(dictionaryLiteral elements: (String, Expression)...) {
+        self.document = Document(dictionaryElements: elements.map {
+            // FIXME: Mapping as a workarond for the compiler being unable to infer the compliance to a protocol
+            ($0.0, $0.1)
+        }).flattened()
     }
 }
--- Sources/MongoKitten/QueryBuilder.swift
+++ Sources/MongoKitten/QueryBuilder.swift
@@ -296,9 +296,14 @@ public indirect enum AQT {
 }
 
 /// A `Query` that consists of an `AQT` statement
-public struct Query: ExpressibleByDictionaryLiteral {
+public struct Query: ExpressibleByDictionaryLiteral, ValueConvertible {
     public init(dictionaryLiteral elements: (String, ValueConvertible)...) {
         self.aqt = .exactly(Document(dictionaryElements: elements))
+    }
+    
+    /// The `Document` that can be sent to the MongoDB Server as a query/filter
+    public func makeBsonValue() -> Value {
+        return self.queryDocument.makeBsonValue()
     }
     
     /// The `Document` that can be sent to the MongoDB Server as a query/filter
@@ -341,11 +346,145 @@ extension Document {
     }
 }
 
-public struct Pipeline {
+public struct Pipeline: ExpressibleByArrayLiteral {
     var document: Document
+    
+    /// A stage in the aggregate
+    public enum Stage: ValueConvertible {
+        /// Takes a `Projection` that defines the inclusions or the exclusion of _id
+        /// 
+        /// For more details: https://docs.mongodb.com/manual/reference/operator/aggregation/project/#pipe._S_project
+        case project(Projection)
+        
+        /// Filters the documents to pass only the documents that match the specified condition(s) to the next pipeline stage as defined in the provided `Query`
+        case match(Query)
+        
+        /// Limits the returned results to the provided `Int` of results
+        ///
+        /// For more details: https://docs.mongodb.com/manual/reference/operator/aggregation/limit/#pipe._S_limit
+        case limit(Int)
+        
+        /// Takes the documents returned by the aggregation pipelien and writes them to a specified collection. This `Stage` must be the last stage in the pipeline.
+        ///
+        /// For more details: https://docs.mongodb.com/manual/reference/operator/aggregation/out/#pipe._S_out
+        case out(collection: String)
+        
+        /// Performs a left outer join to an unsharded collection in the same database to filter in documents from the “joined” collection for processing
+        ///
+        /// fromCollection is an unshaded collection in the same database to perform the join with
+        ///
+        /// localField is the field from the input documents into the lookup stage
+        ///
+        /// foreignField is the field in the `fromCollection`
+        ///
+        /// as is the name of the array to add to the input documents. The array will contain the matching Documents from the `fromCollection` collection. Will overwrite the existing key if there is one.
+        ///
+        /// For more details: https://docs.mongodb.com/manual/reference/operator/aggregation/lookup/#pipe._S_lookup
+        case lookup(fromCollection: String, localfield: String, foreignField: String, as: String)
+        
+        /// Sorts all input documents and puts them in the pipeline in the sorted order
+        ///
+        /// For more details: https://docs.mongodb.com/manual/reference/operator/aggregation/sort/#pipe._S_sort
+        case sort(Sort)
+        
+        /// Randomly selects N Documents from the aggregation pipeline input where N is the inputted size.
+        ///
+        /// For more details: https://docs.mongodb.com/manual/reference/operator/aggregation/sample/#pipe._S_sample
+        case sample(size: Int)
+        
+        /// For more information: https://docs.mongodb.com/manual/reference/operator/aggregation/unwind/#pipe._S_unwind
+        case unwind(path: String, includeArrayIndex: String?, preserveNullAndEmptyBooleans: Bool?)
+        
+        /// Skips over the specified number of documents that pass into the stage and passes the remaining documents to the next stage in the pipeline.
+        case skip(Int)
+        
+        /// Creates a geoNear aggregate with the provided options as described [here](https://docs.mongodb.com/manual/reference/operator/aggregation/geoNear/#pipe._S_geoNear)
+        case geoNear(options: Document)
+        
+        case group(Document)
+        
+        /// Creates a custom aggregate stage using the provided Document
+        ///
+        /// Used for aggregations that MongoKitten does not support
+        case custom(Document)
+        
+        public func makeBsonValue() -> Value {
+            switch self {
+            case .custom(let doc):
+                return doc.makeBsonValue()
+            case .project(let projection):
+                return [
+                    "$project": projection.makeBsonValue()
+                ]
+            case .match(let query):
+                return [
+                    "$match": query
+                ]
+            case .limit(let limit):
+                return [
+                    "$limit": limit
+                ]
+            case .out(let collection):
+                return ["$out": collection]
+            case .lookup(let from, let localField, let foreignField, let namedAs):
+                return ["$lookup": [
+                    "from": from,
+                    "localField": localField,
+                    "foreignField": foreignField,
+                    "as": namedAs
+                    ] as Document
+                ]
+            case .sort(let sort):
+                return ["$sort": sort]
+            case .sample(let size):
+                return ["$sample": [
+                        ["$size": size] as Document
+                    ] as Document
+                ]
+            case .unwind(let path, let includeArrayIndex, let preserveNullAndEmptyArrays):
+                var unwind: Value
+                
+                if let includeArrayIndex = includeArrayIndex {
+                    unwind = [
+                        "path": path
+                    ]
+                    
+                    unwind["includeArrayIndex"] = includeArrayIndex.makeBsonValue()
+                    
+                    if let preserveNullAndEmptyArrays = preserveNullAndEmptyArrays {
+                        unwind["preserveNullAndEmptyArrays"] = preserveNullAndEmptyArrays.makeBsonValue()
+                    }
+                } else if let preserveNullAndEmptyArrays = preserveNullAndEmptyArrays {
+                    unwind = [
+                        "path": path
+                    ]
+                    
+                    unwind["preserveNullAndEmptyArrays"] = preserveNullAndEmptyArrays.makeBsonValue()
+                } else {
+                    unwind = path.makeBsonValue()
+                }
+                
+                return [
+                    "$unwind": unwind
+                ]
+            case .skip(let amount):
+                return ["$skip": amount]
+            case .geoNear(let options):
+                return ["$geoNear": options]
+            case .group(let document):
+                return ["$group": document]
+            }
+        }
+    }
     
     public init(_ document: Document) {
         self.document = document
+    }
+
+    public init(arrayLiteral elements: Stage...) {
+        self.document = Document(array: elements.map {
+            $0
+        })
     }
 }
 
