ae193cf020cf08e0687fb724c5376409f5ac8b3a
Joannis Orlandos <joannis@orlandos.nl>
2016-11-26 14:00:41 +0100

    Extraction/update helpers for CustomValueConvertible

--- Sources/MongoKitten/Document+CustomValueConvertible.swift
+++ Sources/MongoKitten/Document+CustomValueConvertible.swift
@@ -13,7 +13,19 @@ extension Document {
         return V(primitive)
     }
     
+    public func extract<V: CustomValueConvertible>(_ key: [SubscriptExpressionType]) -> V? {
+        guard let primitive = self[raw: key]?.makeBSONPrimitive() else {
+            return nil
+        }
+        
+        return V(primitive)
+    }
+    
     public mutating func updateValue<V: CustomValueConvertible>(_ value: V?, forKey key: SubscriptExpressionType...) {
+        self[raw: key] = value
+    }
+    
+    public mutating func updateValue<V: CustomValueConvertible>(_ value: V?, forKey key: [SubscriptExpressionType]) {
         self[raw: key] = value
     }
 }
