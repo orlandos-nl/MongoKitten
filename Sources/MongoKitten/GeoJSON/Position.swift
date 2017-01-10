178f6eb61f5c780888441c85e8df5e5ef60fdd2e
Joannis Orlandos <joannis@orlandos.nl>
2017-01-10 09:33:18 +0100

    Reversed the position changes.

--- Sources/MongoKitten/GeoJSON/Position.swift
+++ Sources/MongoKitten/GeoJSON/Position.swift
@@ -8,37 +8,52 @@
 
 import Foundation
 
-/// A position with longitude and latitude.
 public struct Position {
-    /// The geo location
-    public let location: (longitude: Double, latitude: Double)
 
-    /// Creates a position on a geo 2d sphere
-    public init(values: (longitude: Double, latitude: Double)) {
-        self.location = values
+    public let values: [Double]
+
+    public init(values: [Double]) {
+        self.values = values
+    }
+
+    public init(first: Double, second: Double, remaining: Double...) {
+        var vals = [Double]()
+        vals.append(first)
+        vals.append(second)
+
+        for current in remaining {
+            vals.append(current)
+        }
+
+        self.values = vals
     }
 }
 
-/// Makes positions equatable
 extension Position: Hashable {
-    /// Checks if two positions are at the same location 
+
     public static func == (lhs: Position, rhs: Position) -> Bool {
-        return lhs.location == rhs.location
+        return lhs.values == rhs.values
     }
 
-    /// A hash value for comparing
+
     public var hashValue: Int {
-        return Int(location.longitude + location.latitude)
+        var value = 0
+
+        for current in values {
+            value = current.hashValue + value
+        }
+
+        return value
     }
 }
 
 extension Position: CustomStringConvertible, CustomDebugStringConvertible {
     public var description: String {
-        return "Position { longitude: \(location.longitude), latitude: \(location.latitude) }"
+        return "Position { values = \(values.description) }"
     }
 
     public var debugDescription: String {
-        return "Position { longitude: \(location.longitude), latitude: \(location.latitude) }"
+        return "Position { values = \(values.debugDescription) }"
     }
 }
 
