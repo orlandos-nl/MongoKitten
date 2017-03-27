2bfb9e290edee513794dfaa0f4c519bf7af814a2
Robbert Brandsma <i@robbertbrandsma.nl>
2017-03-27 14:06:23 +0200

    Updated package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ var package = Package(
         ],
     dependencies: [
         // For MongoDB Documents
-        .Package(url: "https://github.com/OpenKitten/BSON.git", "5.0.0-obbut9"),
+        .Package(url: "https://github.com/OpenKitten/BSON.git", Version(5,0,0, prereleaseIdentifiers: ["alpha", "2"])),
         
         // For ExtendedJSON support
         .Package(url: "https://github.com/OpenKitten/Cheetah.git", majorVersion: 0, minor: 3),
@@ -19,7 +19,7 @@ var package = Package(
         .Package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", Version(0,6,7)),
 
         // Provides Sockets + SSL
-        .Package(url: "https://github.com/vapor/tls.git", "2.0.0-alpha.4"),
+        .Package(url: "https://github.com/vapor/tls.git", Version(2,0,0, prereleaseIdentifiers: ["alpha"])),
         
         // Asynchronous behaviour
         .Package(url: "https://github.com/OpenKitten/Schrodinger.git", majorVersion: 0, minor: 1),
