90757c805810ec567f704f7a512e6456f62451ef
Joannis Orlandos <joannis@orlandos.nl>
2017-04-11 12:03:09 +0200

    Useless test

--- Tests/MongoKittenTests/AuthenticationTests.swift
+++ Tests/MongoKittenTests/AuthenticationTests.swift
@@ -29,12 +29,6 @@ class AuthenticationTests: XCTestCase {
     override func tearDown() {
 
     }
-    
-    func testReplicaSet() throws {
-        let db = try Database("mongodb://openkitten:=my32hQLaf876G@136.144.135.42:27017,136.144.135.41:27017/mongokitten-unittest?ssl=true&sslVerify=false")
-        
-        XCTAssertEqual(try db["zips"].count(), 29_353)
-    }
 
     func testMLabConnection() throws {
         let clientSettings = ClientSettings(host: MongoHost(hostname:"ds015730.mlab.com", port: 15730),sslSettings: nil,credentials: MongoCredentials(username:"kitten",password:"kitten1234"), maxConnectionsPerServer: 20)
