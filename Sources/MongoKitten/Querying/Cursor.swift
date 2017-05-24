e8cb344636fd388e3b39c3e088e25f71110240f3
Joannis Orlandos <joannis@orlandos.nl>
2017-05-24 20:55:22 +0200

    Fixed linux

--- Sources/MongoKitten/Querying/Cursor.swift
+++ Sources/MongoKitten/Querying/Cursor.swift
@@ -8,6 +8,7 @@
 // See https://github.com/OpenKitten/MongoKitten/blob/mongokitten31/CONTRIBUTORS.md for the list of MongoKitten project authors
 //
 
+import Dispatch
 import Foundation
 import BSON
 import Schrodinger
