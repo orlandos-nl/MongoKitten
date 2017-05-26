ec03b56e6233d6f4e61ab302502a53214c127b85
Joannis Orlandos <joannis@orlandos.nl>
2017-05-26 09:50:32 +0200

    Unit tests fix

--- Tests/MongoKittenTests/CollectionTests.swift
+++ Tests/MongoKittenTests/CollectionTests.swift
@@ -685,7 +685,7 @@ public class CollectionTests: XCTestCase {
     
     func testExplain() throws {
         for db in TestManager.dbs {
-            guard self.server.buildInfo.version >= Version(3, 0, 0) else {
+            guard db.server.buildInfo.version >= Version(3, 0, 0) else {
                 continue
             }
             
