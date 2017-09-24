611b2b396d558cbe82b97c599bbc748c06611741
Joannis Orlandos <joannis@orlandos.nl>
2017-09-24 15:32:05 +0200

    keeping kittencore users safe

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ var package = Package(
         ],
     dependencies: [
         // For MongoDB Documents
-        .Package(url: "https://github.com/OpenKitten/BSON.git", versions: Version(5, 1, 2) ..< Version(6, 0, 0)),
+        .Package(url: "https://github.com/OpenKitten/BSON.git", versions: Version(5, 1, 2) ..< Version(5, 2, 0)),
         
         // For ExtendedJSON support
         .Package(url: "https://github.com/OpenKitten/Cheetah.git", majorVersion: 1),
