5e586cb89cc2bedaf9e570c92ec7a057c64e5dbe
Igor Voynov <igyo@me.com>
2018-04-16 00:58:38 +0300

    Added key field for create indexes

--- Sources/MongoKitten/Deprecated/Deprecated.swift
+++ Sources/MongoKitten/Deprecated/Deprecated.swift
@@ -46,4 +46,59 @@ extension Collection {
             throw MongoError.commandFailure(error: document)
         }
     }
+    
+    /// Creates an `Index` in this `Collection` on the specified keys.
+    ///
+    /// Usage:
+    ///
+    /// ```swift
+    /// // Makes "username" unique and indexed. Sort order doesn't technically matter much
+    /// try collection.createIndex(named: "login", .sort(field: "username", order: .ascending), .unique)
+    /// ```
+    ///
+    /// For more information: https://docs.mongodb.com/manual/reference/command/createIndexes/#dbcmd.createIndexes
+    ///
+    /// - parameter name: The name of this index used to identify it
+    /// - parameter parameters: All `IndexParameter` options applied to the index
+    ///
+    /// - throws: When unable to send the request/receive the response, the authenticated user doesn't have sufficient permissions or an error occurred
+    @available(*, deprecated: 4.1.3, message: "The index key is a required field")
+    public func createIndex(named name: String, withParameters parameters: IndexParameter...) throws {
+        try self.createIndexes([(name: name, parameters: parameters)])
+    }
+    
+    /// Creates multiple indexes as specified
+    ///
+    /// For more information: https://docs.mongodb.com/manual/reference/command/createIndexes/#dbcmd.createIndexes
+    ///
+    /// - parameter indexes: The indexes to create. Accepts an array of tuples (each tuple representing an Index) which an contain a name and always contains an array of `IndexParameter`.
+    ///
+    /// - throws: When unable to send the request/receive the response, the authenticated user doesn't have sufficient permissions or an error occurred
+    @available(*, deprecated: 4.1.3, message: "The index key is a required field")
+    public func createIndexes(_ indexes: [(name: String, parameters: [IndexParameter])]) throws {
+        guard let wireVersion = database.server.serverData?.maxWireVersion , wireVersion >= 2 else {
+            throw MongoError.unsupportedOperations
+        }
+        
+        var indexDocs = [Document]()
+        
+        for index in indexes {
+            var indexDocument: Document = [
+                "name": index.name
+            ]
+            
+            for parameter in index.parameters {
+                indexDocument += parameter.document
+            }
+            
+            indexDocs.append(indexDocument)
+        }
+        
+        
+        let document = try firstDocument(in: try database.execute(command: ["createIndexes": self.name, "indexes": Document(array: indexDocs)]))
+        
+        guard Int(document["ok"]) == 1 else {
+            throw MongoError.commandFailure(error: document)
+        }
+    }
 }
