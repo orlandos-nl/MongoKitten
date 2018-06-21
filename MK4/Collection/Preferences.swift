1f71c89cf8ca96824c0be33b971c169be969a388
Robbert Brandsma <i@robbertbrandsma.nl>
2018-06-21 11:47:24 +0200

    Removed junk

--- MK4/Collection/Preferences.swift
+++
@@ -1,7 +0,0 @@
-public struct Preferences {
-    public var readConcern: ReadConcern?
-    public var writeConcern: WriteConcern?
-    public var collation: Collation?
-    
-    public init() {}
-}
