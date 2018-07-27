b89a1539b49525eb71089bd9c13bc6db6edb4401
Robbert Brandsma <i@robbertbrandsma.nl>
2018-07-28 00:48:43 +0200

    GridFS indexes

--- Sources/GridFS/GridFS.swift
+++ Sources/GridFS/GridFS.swift
@@ -12,15 +12,15 @@ public class GridFS {
     let filesCollection: MongoKitten.Collection
     let chunksCollection: MongoKitten.Collection
     
-    private init(named name: String, in database: Database) {
-        self.filesCollection = database["\(name).files"]
-        self.chunksCollection = database["\(name).chunks"]
+    private var didEnsureIndexes: Bool = false
+    
+    var eventLoop: EventLoop {
+        return filesCollection.database.connection.eventLoop
     }
     
-    /// Returns a GridFS bucket with the given `name` in the given `database`.
-    public static func `in`(database: Database, named name: String = "fs") -> EventLoopFuture<GridFS> {
-        // TODO: Ensure indexes
-        return database.connection.eventLoop.newSucceededFuture(result: GridFS(named: name, in: database))
+    public init(named name: String, in database: Database) {
+        self.filesCollection = database["\(name).files"]
+        self.chunksCollection = database["\(name).chunks"]
     }
     
     public func find(_ query: Query) -> FileCursor {
@@ -46,5 +46,38 @@ public class GridFS {
     
     // TODO: Cancellable, streaming writes & reads
     // TODO: Non-streaming writes & reads
+    
+    internal func ensureIndexes() -> EventLoopFuture<Void> {
+        guard !didEnsureIndexes else {
+            return eventLoop.newSucceededFuture(result: ())
+        }
+        
+        didEnsureIndexes = true
+        
+        return filesCollection
+            .find()
+            .project(["_id": .included])
+            .limit(1)
+            .getFirstResult()
+            .then { result in
+                // Determine if the files collection is empty
+                guard result == nil else {
+                    return self.eventLoop.newSucceededFuture(result: ())
+                }
+                
+                // TODO: Drivers MUST check whether the indexes already exist before attempting to create them. This supports the scenario where an application is running with read-only authorizations.
+                
+                return EventLoopFuture<Void>.andAll([
+                    self.filesCollection.indexes.createCompound(named: "mongokitten_was_here", keys: [
+                        "filename": .ascending,
+                        "uploadDate": .ascending
+                        ]),
+                    self.chunksCollection.indexes.createCompound(named: "mongokitten_was_here", keys: [
+                        "files_id": .ascending,
+                        "n": .ascending
+                        ], options: [.unique])
+                    ], eventLoop: self.eventLoop)
+        }
+    }
     
 }
