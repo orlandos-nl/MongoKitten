cdeb79bbc93ff33800e48b4f86bda4760ae44210
Joannis Orlandos <joannis@orlandos.nl>
2019-07-07 20:51:40 +0200

    Export MongoClient

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ var package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "MongoKitten",
-            targets: ["MongoKitten"]),
+            targets: ["MongoKitten", "MongoClient"]),
         .library(
             name: "MongoClient",
             targets: ["MongoClient"]),
