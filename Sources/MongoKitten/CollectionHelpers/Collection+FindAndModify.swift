6a76862dbd6a73cd054370dfd64c70739c460491
Andrew Edwards <andrewangeta@gmail.com>
2020-04-10 11:43:10 -0400

    Removed convenience functions.

--- Sources/MongoKitten/CollectionHelpers/Collection+FindAndModify.swift
+++ Sources/MongoKitten/CollectionHelpers/Collection+FindAndModify.swift
@@ -3,67 +3,6 @@ import MongoClient
 import MongoKittenCore
 
 extension MongoCollection {
-    
-    // MARK: - Convenience Functions (Simple API)
-    
-    /// Modifies and returns a single document.
-    /// - Parameters:
-    ///   - where: The selection criteria for the modification.
-    ///   - update: If passed a document with update operator expressions, performs the specified modification. If passed a replacement document performs a replacement.
-    ///   - remove: Removes the document specified in the query field. Defaults to `false`
-    ///   - returnValue: Wether to return the `original` or `modified` document.
-    public func findAndModify(where query: Document,
-                              update document: Document = [:],
-                              remove: Bool = false,
-                              returnValue: FindAndModifyReturnValue = .original) -> EventLoopFuture<FindAndModifyReply> {
-        var command = FindAndModifyCommand(collection: self.name, query: query)
-        command.update = document
-        command.remove = remove
-        command.new = returnValue == .modified
-        return FindAndModifyBuilder(command: command, collection: self).execute()
-    }
-    
-    /// Deletes a single document based on the query, returning the deleted document.
-    /// - Parameters:
-    ///   - query: The selection criteria for the deletion.
-    public func findOneAndDelete(where query: Document) -> EventLoopFuture<Document?> {
-        var command = FindAndModifyCommand(collection: self.name, query: query)
-        command.remove = true
-        return FindAndModifyBuilder(command: command, collection: self)
-        .execute()
-        .map { $0.value }
-    }
-    
-    /// Replaces a single document based on the specified query.
-    /// - Parameters:
-    ///   - query: The selection criteria for the upate.
-    ///   - replacement: The replacement document.
-    ///   - returnValue: Wether to return the `original` or `modified` document.
-    public func findOneAndReplace(where query: Document,
-                                  replacement document: Document,
-                                  returnValue: FindAndModifyReturnValue = .original) -> EventLoopFuture<FindAndModifyReply> {
-        var command = FindAndModifyCommand(collection: self.name, query: query)
-        command.new = returnValue == .modified
-        command.update = document
-        return FindAndModifyBuilder(command: command, collection: self).execute()
-    }
-    
-    /// Updates a single document based on the specified query.
-    /// - Parameters:
-    ///   - query: The selection criteria for the upate.
-    ///   - document: The update document.
-    ///   - returnValue: Wether to return the `original` or `modified` document.
-    public func findOneAndUpdate(where query: Document,
-                                 to document: Document,
-                                 returnValue: FindAndModifyReturnValue = .original) -> EventLoopFuture<Document?> {
-        var command = FindAndModifyCommand(collection: self.name, query: query)
-        command.new = returnValue == .modified
-        command.update = document
-        return FindAndModifyBuilder(command: command, collection: self)
-        .execute()
-        .map { $0.value }
-    }
-    
     // MARK: - Builder Functions (Composable/Chained API)
     
     /// Modifies and returns a single document.
