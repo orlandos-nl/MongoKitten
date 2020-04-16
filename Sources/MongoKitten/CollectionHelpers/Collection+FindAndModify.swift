e552b4e11f894c17ab8f6cb06886c6c1325ce32c
Andrew Edwards <andrewangeta@gmail.com>
2020-04-15 23:28:36 -0400

    Added mongo query helpers.

--- Sources/MongoKitten/CollectionHelpers/Collection+FindAndModify.swift
+++ Sources/MongoKitten/CollectionHelpers/Collection+FindAndModify.swift
@@ -7,7 +7,7 @@ extension MongoCollection {
     
     /// Modifies and returns a single document.
     /// - Parameters:
-    ///   - where: The selection criteria for the modification.
+    ///   - query: The selection criteria for the modification.
     ///   - update: If passed a document with update operator expressions, performs the specified modification. If passed a replacement document performs a replacement.
     ///   - remove: Removes the document specified in the query field. Defaults to `false`
     ///   - returnValue: Wether to return the `original` or `modified` document.
@@ -54,6 +54,60 @@ extension MongoCollection {
                                  to document: Document,
                                  returnValue: FindAndModifyReturnValue = .original) -> FindAndModifyBuilder {
         var command = FindAndModifyCommand(collection: self.name, query: query)
+        command.new = returnValue == .modified
+        command.update = document
+        return FindAndModifyBuilder(command: command, collection: self)
+    }
+    
+    /// Modifies and returns a single document.
+    /// - Parameters:
+    ///   - query: The selection criteria for the modification.
+    ///   - update: If passed a document with update operator expressions, performs the specified modification. If passed a replacement document performs a replacement.
+    ///   - remove: Removes the document specified in the query field. Defaults to `false`
+    ///   - returnValue: Wether to return the `original` or `modified` document.
+    public func findAndModify<Query: MongoKittenQuery>(where query: Query,
+                                                       update document: Document = [:],
+                                                       remove: Bool = false,
+                                                       returnValue: FindAndModifyReturnValue = .original) -> FindAndModifyBuilder {
+        var command = FindAndModifyCommand(collection: self.name, query: query.makeDocument())
+        command.update = document
+        command.remove = remove
+        command.new = returnValue == .modified
+        return FindAndModifyBuilder(command: command, collection: self)
+    }
+    
+    /// Deletes a single document based on the query, returning the deleted document.
+    /// - Parameters:
+    ///   - query: The selection criteria for the deletion.
+    public func findOneAndDelete<Query: MongoKittenQuery>(where query: Query) -> FindAndModifyBuilder {
+        var command = FindAndModifyCommand(collection: self.name, query: query.makeDocument())
+        command.remove = true
+        return FindAndModifyBuilder(command: command, collection: self)
+    }
+    
+    /// Replaces a single document based on the specified query.
+    /// - Parameters:
+    ///   - query: The selection criteria for the upate.
+    ///   - replacement: The replacement document.
+    ///   - returnValue: Wether to return the `original` or `modified` document.
+    public func findOneAndReplace<Query: MongoKittenQuery>(where query: Query,
+                                                           replacement document: Document,
+                                                           returnValue: FindAndModifyReturnValue = .original) -> FindAndModifyBuilder {
+        var command = FindAndModifyCommand(collection: self.name, query: query.makeDocument())
+        command.new = returnValue == .modified
+        command.update = document
+        return FindAndModifyBuilder(command: command, collection: self)
+    }
+    
+    /// Updates a single document based on the specified query.
+    /// - Parameters:
+    ///   - query: The selection criteria for the upate.
+    ///   - document: The update document.
+    ///   - returnValue: Wether to return the `original` or `modified` document.
+    public func findOneAndUpdate<Query: MongoKittenQuery>(where query: Query,
+                                                          to document: Document,
+                                                          returnValue: FindAndModifyReturnValue = .original) -> FindAndModifyBuilder {
+        var command = FindAndModifyCommand(collection: self.name, query: query.makeDocument())
         command.new = returnValue == .modified
         command.update = document
         return FindAndModifyBuilder(command: command, collection: self)
