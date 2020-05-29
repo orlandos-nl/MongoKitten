bfa1aa5e5ccd1d9af86708be29918b27b08b650b
Anton Remizov <anton@calthings.com>
2020-05-29 15:45:35 -0700

    Fix tls cert

--- Sources/MongoClient/Connection.swift
+++ Sources/MongoClient/Connection.swift
@@ -117,7 +117,7 @@ public final class MongoConnection {
         return bootstrap
             .channelOption(ChannelOptions.socket(SocketOptionLevel(SOL_SOCKET), SO_REUSEADDR), value: 1)
             .channelInitializer { channel in
-                #if !canImport(NIOTransportServices)
+                #if canImport(NIOTransportServices)
                 if settings.useSSL {
                     do {
                         var configuration = TLSConfiguration.clientDefault
