bb9bde1df75add3585af9a6a3d023a5838920196
Joannis Orlandos <joannis@orlandos.nl>
2020-05-29 14:23:13 +0200

    Only support ios 12+

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,7 +7,7 @@ let package = Package(
     name: "MongoKitten",
     platforms: [
         .macOS(.v10_14),
-        .ios(.v12),
+        .iOS(.v12),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
