031597e298e992e91879eef2b4e1ebbf9148e47c
Andrew Edwards <andrewangeta@gmail.com>
2020-06-18 17:56:34 -0400

    Added delete helpers that support multiple delete commands.

--- Sources/MongoKitten/CollectionHelpers/Collection+Delete.swift
+++ Sources/MongoKitten/CollectionHelpers/Collection+Delete.swift
@@ -31,4 +31,23 @@ extension MongoCollection {
     public func deleteAll<Q: MongoKittenQuery>(where filter: Q) -> EventLoopFuture<DeleteReply> {
         return self.deleteAll(where: filter.makeDocument())
     }
+    
+    /// Performs a delete operation with the given delete statements
+    /// - Parameter removals: A collection of one or more delete statements to perform.
+    public func deleteAll(_ removals: [DeleteCommand.Removal]) -> EventLoopFuture<DeleteReply> {
+        return pool.next(for: .writable).flatMap { connection in
+            return connection.executeCodable(
+                DeleteCommand(removals, fromCollection: self.name),
+                namespace: self.database.commandNamespace,
+                in: self.transaction,
+                sessionId: connection.implicitSessionId
+            )
+        }.decodeReply(DeleteReply.self)._mongoHop(to: hoppedEventLoop)
+    }
+    
+    /// Performs a delete operation with the given delete statement.
+    /// - Parameter removal: A delete statement to perform.
+    public func deleteOne(_ removal: DeleteCommand.Removal) -> EventLoopFuture<DeleteReply> {
+        return self.deleteAll([removal])
+    }
 }
