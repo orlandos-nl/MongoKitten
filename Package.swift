b8d4b3fb64dab94e94e76347d011845777cb3e1a
Joannis Orlandos <joannis@orlandos.nl>
2020-08-08 19:19:06 +0200

    Support iOS 12+

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "MongoKitten",
     platforms: [
-        .macOS(.v10_14)
+        .macOS(.v10_14),
+        .iOS(.v12)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
