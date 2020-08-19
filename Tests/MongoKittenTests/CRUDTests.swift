e88a0394200874399300f69c77e2c4301ae64f97
Spirit Balanceholder <martinkleinpaste@hotmail.com>
2020-08-19 12:55:21 +0200

    make tests dummy data agnostic

--- Tests/MongoKittenTests/CRUDTests.swift
+++ Tests/MongoKittenTests/CRUDTests.swift
@@ -6,7 +6,7 @@ import MongoCore
 let dbName = "KittenTest"
 
 class CrudTests : XCTestCase {
-    struct DummyAccount: Codable {
+    struct DummyAccount: Codable, Equatable {
         static let collectionName = "DummyAccounts"
         
         let _id: ObjectId
@@ -18,6 +18,22 @@ class CrudTests : XCTestCase {
           self._id = ObjectId()
           self.name = name
           self.password = password
+          self.age = age
+        }
+    }
+
+    struct DummyAccountAlt: Codable, Equatable {
+        static let collectionName = "DummyAccounts"
+        
+        let _id: ObjectId
+        var firstName: String
+        var lastName: String
+        var age: Int
+        
+        init(firstName: String, lastName: String, age: Int) {
+          self._id = ObjectId()
+          self.firstName = firstName
+          self.lastName = lastName
           self.age = age
         }
     }
@@ -63,12 +79,12 @@ class CrudTests : XCTestCase {
         let startingCount: Int = try schema.count().wait()
         
         let dummyAccount = DummyAccount(name: "Dum", password: "my", age: 69)
-        let _ = try schema.insertEncoded(dummyAccount).wait()
+        _ = try schema.insertEncoded(dummyAccount).wait()
         XCTAssertEqual(try schema.count().wait(), startingCount+1)
     }
     
     // is used repeatedly for all the tests that require dummy data
-    func testBulkCreateDummyAccounts () throws -> [DummyAccount] {
+    func testBackupBulkCreateDummyAccounts () throws -> [DummyAccount] {
         let dummyAccounts = [
             DummyAccount(name: "Test", password: "ing", age: 77),
             DummyAccount(name: "To", password: "see", age: 10),
@@ -92,22 +108,53 @@ class CrudTests : XCTestCase {
         XCTAssertEqual(insertReply.insertCount, dummyAccounts.count)
         return dummyAccounts
     }
+
+    // is used repeatedly for some of the tests that require large dummy data
+    func testBulkCreateDummyAccounts () throws -> [DummyAccount] {
+        var dummyAccounts: [DummyAccount] = []
+
+        for index in 0...4 {
+            for age in 1...100 {
+                dummyAccounts.append(DummyAccount(name: "Name-\(age + index * 100)", password: "Pass-\(age + index * 100)", age: age))
+            }
+        }
+
+        let documents: [Document] = try dummyAccounts.map { dummyAccount in
+            try BSONEncoder().encode(dummyAccount)
+        }
+        
+        var command: InsertCommand = InsertCommand(documents: documents, inCollection: DummyAccount.collectionName)
+        command.writeConcern = concern
+        let connection: MongoConnection = try mongo.pool.next(for: .init(writable: false)).wait()
+        let insertReply: InsertReply = try connection.executeCodable(command, namespace: mongo.commandNamespace, in: nil, sessionId: mongo.sessionId).decodeReply(InsertReply.self).wait()
+        
+        XCTAssertEqual(insertReply.ok, 1)
+        XCTAssertEqual(insertReply.insertCount, dummyAccounts.count)
+        return dummyAccounts
+    }
     
     func readDummyAccount (name : String) throws -> DummyAccount? {
         let schema: MongoCollection = mongo[DummyAccount.collectionName]
         return try schema.findOne("name" == name, as: DummyAccount.self).wait()
     }
-    
+
     func testReadDummyAccounts () throws {
         try _ = testBulkCreateDummyAccounts()
-        if let dummy: DummyAccount = try readDummyAccount(name: "Them") {
-            XCTAssertEqual(dummy.password, "to")
+        if let dummy: DummyAccount = try readDummyAccount(name: "Name-182") {
+            XCTAssertEqual(dummy.password, "Pass-182")
             XCTAssertEqual(dummy.age, 82 )
         } else {
             XCTFail("Retrieved a nil value")
         }
     }
     
+    func testReadLargeBulkAccounts () throws {
+        let dummyAccounts: [DummyAccount] = try testBulkCreateDummyAccounts()
+        let schema: MongoCollection = mongo[DummyAccount.collectionName]
+        let age50Duplicates = try schema.find("age" > 50).decode(DummyAccount.self).allResults().wait()
+        XCTAssertEqual(age50Duplicates.count, dummyAccounts.filter{$0.age > 50}.count)
+    }
+
     func testReadDistictAge () throws {
         let dummyAccounts: [DummyAccount] = try testBulkCreateDummyAccounts()
         let schema: MongoCollection = mongo[DummyAccount.collectionName]
@@ -179,7 +226,7 @@ class CrudTests : XCTestCase {
     }
 
     func testBulkReadDummyAccounts () throws {
-        try _ = testBulkCreateDummyAccounts()
+        let dummyAccounts = try testBulkCreateDummyAccounts()
         let schema: MongoCollection = mongo[DummyAccount.collectionName]
         var dummyCounter = 0
         
@@ -187,21 +234,23 @@ class CrudTests : XCTestCase {
             XCTAssertNotNil(dummy)
             dummyCounter+=1
         }.wait()
-        XCTAssertEqual(dummyCounter, 7)
+        XCTAssertEqual(dummyCounter, dummyAccounts.count)
     }
     
     func testUpdateDummyAccounts () throws {
-        try _ = testBulkCreateDummyAccounts()
-        if var dummy = try readDummyAccount(name: "Them") {
+        let testDummyAccounts = try testBulkCreateDummyAccounts()
+        let testDummyAccount = testDummyAccounts[Int.random(in: 0...testDummyAccounts.count)]
 
+        if var account = try readDummyAccount(name: testDummyAccount.name) {
             let schema: MongoCollection = mongo[DummyAccount.collectionName]
-            dummy.name = "UpdateTest"
 
-            _ = try schema.updateEncoded(where: "_id" == dummy._id, to: dummy).wait()
-            let updatedDummy: DummyAccount? = try readDummyAccount(name: "UpdateTest")
+            account.name = "UpdateTest"
 
-            XCTAssertNotNil(updatedDummy)
-            XCTAssertEqual(updatedDummy?._id, dummy._id)
+            _ = try schema.updateEncoded(where: "_id" == account._id, to: account).wait()
+            let updatedAccount: DummyAccount? = try readDummyAccount(name: "UpdateTest")
+
+            XCTAssertNotNil(updatedAccount)
+            XCTAssertEqual(updatedAccount?._id, account._id)
         } else {
             XCTFail("Retrieved a nil value")
         }
@@ -219,9 +268,10 @@ class CrudTests : XCTestCase {
     }
     
     func testDeleteDummyAccounts () throws {
-        try _ = testBulkCreateDummyAccounts()
+        let testDummyAccounts = try testBulkCreateDummyAccounts()
+        let testDummyAccount = testDummyAccounts[Int.random(in: 0...testDummyAccounts.count)]
         
-        if let dummy = try readDummyAccount(name: "Them") {
+        if let dummy = try readDummyAccount(name: testDummyAccount.name) {
             let schema: MongoCollection = mongo[DummyAccount.collectionName]
             let deleteReply: DeleteReply = try schema.deleteOne(where: "_id" == dummy._id).wait()
 
@@ -231,33 +281,57 @@ class CrudTests : XCTestCase {
 
             sleep(5)
 
-            XCTAssertNil(try readDummyAccount(name: "Them"))
+            XCTAssertNil(try readDummyAccount(name: testDummyAccount.name))
         } else {
-            XCTFail("Retrieved a nil value")
+            XCTFail()
         }
     }
     
     func testBulkDeleteDummyAccounts () throws {
-        try _ = testBulkCreateDummyAccounts()
+        let testDummyAccounts = try testBulkCreateDummyAccounts().filter{$0.age >= 18}
         
         let schema: MongoCollection = mongo[DummyAccount.collectionName]
         _ = try schema.deleteAll(where: "age" < 18).wait()
         
-        XCTAssertEqual(try schema.count().wait(), 4)
+        XCTAssertEqual(try schema.count().wait(), testDummyAccounts.count)
     }
 
     func testAggregate () throws {
         try _ = testBulkCreateDummyAccounts()
         let schema: MongoCollection = mongo[DummyAccount.collectionName]
-        let AggregateBuilderStages: [AggregateBuilderStage] = [
+        let aggregateBuilderStages: [AggregateBuilderStage] = [
             AggregateBuilderStage.match("age" >= 18),
             AggregateBuilderStage.sort(["age": .ascending]),
             AggregateBuilderStage.limit(3)
         ]
-        let results = try schema.aggregate(AggregateBuilderStages).decode(DummyAccount.self).allResults().wait()
-        XCTAssertEqual(results[0].age, 19)
-        XCTAssertEqual(results[1].age, 30)
-        XCTAssertEqual(results[2].age, 77)
+        let results = try schema.aggregate(aggregateBuilderStages).decode(DummyAccount.self).allResults().wait()
+        XCTAssertEqual(results.count, 3)
+        XCTAssertEqual(results.filter{$0.age >= 18}.count, 3)
+        for index in 1 ..< results.count {
+            guard results[index-1].age <= results[index].age else {
+                XCTFail()
+                return
+            }
+        }
+    }
+
+    func testAggregateBuilder () throws {
+        try _ = testBulkCreateDummyAccounts()
+        let schema: MongoCollection = mongo[DummyAccount.collectionName]
+        let results = try schema.buildAggregate{
+            match("age" >= 18)
+            sort(["age": .ascending])
+            limit(3)
+        }.decode(DummyAccount.self).allResults().wait()
+
+        XCTAssertEqual(results.count, 3)
+        XCTAssertEqual(results.filter{$0.age >= 18}.count, 3)
+        for index in 1 ..< results.count {
+            guard results[index-1].age <= results[index].age else {
+                XCTFail()
+                return
+            }
+        }
     }
 
     func testListCollections () throws {
@@ -269,13 +343,11 @@ class CrudTests : XCTestCase {
         let badSettings = try! ConnectionSettings("mongodb+srv://AMTester:Autimatisering1@0.0.0.0/\(dbName)?retryWrites=true&w=majority")
         do {
             _ = try MongoDatabase.synchronousConnect(settings: badSettings)
+            XCTFail()
         } catch {
-            return
         }
-        XCTFail()
     }
     
-    // TODO: adds minimal contribution, remove?
     func testIllegalInsert () throws {
         _ = try testBulkCreateDummyAccounts()
         let schema: MongoCollection = mongo[DummyAccount.collectionName]
@@ -288,20 +360,12 @@ class CrudTests : XCTestCase {
         XCTAssertEqual(reply.writeErrors?[0].code, 11000)
     }
 
-    // TODO: Does not raise coverage score, remove? 
-    //
+    // TODO: SequentialForEach
     // func testCursorLoop () throws {
-    //     _ = try testBulkCreateDummyAccounts()
+    //     let dummyAccounts = try testBulkCreateDummyAccounts()
     //     let schema: MongoCollection = mongo[DummyAccount.collectionName]
-    //     var cursorDummyAccounts: [DummyAccount] = []
-    //     try schema.find().forEach { (account: Document) -> Void in 
-    //         cursorDummyAccounts.append(try BSONDecoder().decode(DummyAccount.self, from: account))
-    //     }.wait()
-    //     XCTAssertGreaterThan(cursorDummyAccounts.count, 0)
+    //     let bla = try schema.find().sequentialForEach{}
     // }
-
-
-    //TODO: ask yoanis about testfile and constrainsts surrounding it
     
     func testGridFSInsert () throws {
         let file = Data(repeating: 0x50, count: 2_000_000)
@@ -313,5 +377,61 @@ class CrudTests : XCTestCase {
         }
     }
 
+    func testFailableAllResults () throws {
+        let testDummyAccounts = try testBulkCreateDummyAccounts()
+        let schema: MongoCollection = mongo[DummyAccount.collectionName]
+        
+        let dummyAccount = DummyAccountAlt(firstName: "OddOne", lastName: "Out", age: 69)
+        _ = try schema.insertEncoded(dummyAccount).wait()
+        
+        let accounts = try schema.find().decode(DummyAccount.self).allResults(failable: true).wait()
+        let accountsAlt = try schema.find().decode(DummyAccountAlt.self).allResults(failable: true).wait()
+
+        XCTAssertEqual(accounts.count, testDummyAccounts.count)
+        XCTAssertEqual(accountsAlt.count, 1)
+    }
+
+    func testFindUpdate () throws {
+        let originalAccounts = try testBulkCreateDummyAccounts()
+        let schema: MongoCollection = mongo[DummyAccount.collectionName]
+
+        let results = try schema.findAndModify(
+            where: "age" == 10, 
+            update: ["$set": ["age": 111]], 
+            returnValue: FindAndModifyReturnValue.original)
+            .execute().wait()
+        
+        guard let resultValue = results.value else{
+            XCTFail()
+            return
+        }
+
+        let originalAccount = try BSONDecoder().decode(DummyAccount.self, from: resultValue)
+        let newAccount = try schema.findOne("age" == 111).decode(DummyAccount.self).wait()
+        XCTAssert(originalAccounts.contains(originalAccount))
+        XCTAssertEqual(newAccount?.age, 111)
+    }
+
+    // func testFindOneAndDelete () throws {
+    //     let originalAccounts = try testBulkCreateDummyAccounts()
+    //     let schema: MongoCollection = mongo[DummyAccount.collectionName]
+
+    //     let results = try schema.findOneAndDelete(where: ["age": 30]).execute().wait()
+        
+    //     guard let resultValue = results.value else{
+    //         XCTFail()
+    //         return
+    //     }
+
+    //     let Account = try BSONDecoder().decode(DummyAccount.self, from: resultValue)
+    //     XCTAssert(originalAccounts.contains(Account))
+    //     schema.findOneAndDelete(where: ["age": 30]).execute().wait()
+    // }
+
+    // TODO: Foreach future
+    // TODO: Change stream
+    // TODO: Find varieties
+    // TODO: Indexes
+    // TODO: Transactions
 
 }
