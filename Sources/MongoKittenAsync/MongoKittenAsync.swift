23860614253f25f659260c9f399e1dc8c5975057
Joannis orlandos <joannis@orlandos.nl>
2021-07-02 13:21:01 +0200

    Discussing with Obbut

--- Sources/MongoKittenAsync/MongoKittenAsync.swift
+++ Sources/MongoKittenAsync/MongoKittenAsync.swift
@@ -3,229 +3,235 @@ import NIO
 import MongoClient
 import MongoKitten
 
-public struct MongoCluster {
-    private let cluster: MongoKitten.MongoCluster
-    public var heartbeatFrequency: TimeAmount {
-        get { cluster.heartbeatFrequency }
-        nonmutating set { cluster.heartbeatFrequency = newValue }
-    }
-    
-    public var wireVersion: WireVersion? {
-        cluster.wireVersion
-    }
-    
-    public init(connectionString: String, awaitDiscovery: Bool) async throws {
-        let settings = try ConnectionSettings(connectionString)
-        cluster = try MongoKitten.MongoCluster(lazyConnectingTo: settings, on: MultiThreadedEventLoopGroup(numberOfThreads: 1))
+extension MongoCluster {
+    public struct Async {
+        public let nio: MongoCluster
+        public var heartbeatFrequency: TimeAmount {
+            get { nio.heartbeatFrequency }
+            nonmutating set { nio.heartbeatFrequency = newValue }
+        }
         
-        try await cluster.initialDiscovery.get()
-    }
-    
-    public subscript(db: String) -> MongoDatabase {
-        MongoDatabase(db: cluster[db])
-    }
-    
-    public func onRediscover(perform: @escaping () -> ()) {
-        self.cluster.didRediscover = perform
-    }
-    
-    public func disconnect() async throws {
-        try await cluster.disconnect().get()
-    }
-    
-    public func reconnect() async throws {
-        try await cluster.reconnect().get()
-    }
-    
-    public func listDatabases() async throws -> [MongoDatabase] {
-        return try await cluster.listDatabases().get().map(MongoDatabase.init)
-    }
-}
-
-public struct MongoDatabase {
-    fileprivate let db: MongoKitten.MongoDatabase
-    
-    public var name: String { db.name }
-    public var isInTransaction: Bool { db.isInTransaction }
-    
-    public func listCollections() async throws -> [MongoCollection] {
-        return try await db.listCollections().get().map(MongoCollection.init)
-    }
-    
-    public subscript(collection: String) -> MongoCollection {
-        MongoCollection(collection: db[collection])
-    }
-    
-    public func drop() async throws {
-        try await db.drop().get()
+        public var wireVersion: WireVersion? {
+            nio.wireVersion
+        }
+        
+        public init(connectionString: String, awaitDiscovery: Bool) async throws {
+            let settings = try ConnectionSettings(connectionString)
+            nio = try MongoCluster(lazyConnectingTo: settings, on: MultiThreadedEventLoopGroup(numberOfThreads: 1))
+            
+            try await nio.initialDiscovery.get()
+        }
+        
+        public subscript(db: String) -> MongoDatabase.Async {
+            MongoDatabase.Async(nio: nio[db])
+        }
+        
+        public func onRediscover(perform: @escaping () -> ()) {
+            self.nio.didRediscover = perform
+        }
+        
+        public func disconnect() async throws {
+            try await nio.disconnect().get()
+        }
+        
+        public func reconnect() async throws {
+            try await nio.reconnect().get()
+        }
+        
+        public func listDatabases() async throws -> [MongoDatabase.Async] {
+            return try await nio.listDatabases().get().map(MongoDatabase.Async.init)
+        }
     }
 }
 
-public struct MongoCollection {
-    fileprivate let collection: MongoKitten.MongoCollection
-    
-    public var name: String { collection.name }
-    public var isInTransaction: Bool { collection.isInTransaction }
-    
-    public func drop() async throws {
-        try await collection.drop().get()
+extension MongoDatabase {
+    public struct Async {
+        public let nio: MongoDatabase
+        
+        public var name: String { nio.name }
+        public var isInTransaction: Bool { nio.isInTransaction }
+        
+        public func listCollections() async throws -> [MongoCollection.Async] {
+            return try await nio.listCollections().get().map(MongoCollection.Async.init)
+        }
+        
+        public subscript(collection: String) -> MongoCollection.Async {
+            MongoCollection.Async(nio: nio[collection])
+        }
+        
+        public func drop() async throws {
+            try await nio.drop().get()
+        }
     }
-    
-    public func count(_ query: Document? = nil) async throws -> Int {
-        try await collection.count(query).get()
-    }
-    
-    public func count<Query: MongoKittenQuery>(_ query: Query? = nil) async throws -> Int {
-        try await collection.count(query).get()
-    }
-    
-    @discardableResult
-    public func deleteOne(where query: Document) async throws -> DeleteReply {
-        try await collection.deleteOne(where: query).get()
-    }
-    
-    @discardableResult
-    public func deleteOne<Query: MongoKittenQuery>(where query: Query) async throws -> DeleteReply {
-        try await collection.deleteOne(where: query).get()
-    }
-    
-    @discardableResult
-    public func deleteAll(where query: Document) async throws -> DeleteReply {
-        try await collection.deleteAll(where: query).get()
-    }
-    
-    @discardableResult
-    public func deleteAll<Query: MongoKittenQuery>(where query: Query) async throws -> DeleteReply {
-        try await collection.deleteAll(where: query).get()
-    }
-    
-    public func find(_ query: Document = [:]) -> FindQueryBuilder {
-        collection.find(query)
-    }
-    
-    public func find<Query: MongoKittenQuery>(_ query: Query) -> FindQueryBuilder {
-        collection.find(query)
-    }
+}
 
-    public func find<D: Decodable>(_ query: Document = [:], as type: D.Type) -> MappedCursor<FindQueryBuilder, D> {
-        collection.find(query, as: type)
-    }
+extension MongoCollection {
+    public struct Async {
+        public let nio: MongoCollection
+        
+        public var name: String { nio.name }
+        public var isInTransaction: Bool { nio.isInTransaction }
+        
+        public func drop() async throws {
+            try await nio.drop().get()
+        }
+        
+        public func count(_ query: Document? = nil) async throws -> Int {
+            try await nio.count(query).get()
+        }
+        
+        public func count<Query: MongoKittenQuery>(_ query: Query? = nil) async throws -> Int {
+            try await nio.count(query).get()
+        }
+        
+        @discardableResult
+        public func deleteOne(where query: Document) async throws -> DeleteReply {
+            try await nio.deleteOne(where: query).get()
+        }
+        
+        @discardableResult
+        public func deleteOne<Query: MongoKittenQuery>(where query: Query) async throws -> DeleteReply {
+            try await nio.deleteOne(where: query).get()
+        }
+        
+        @discardableResult
+        public func deleteAll(where query: Document) async throws -> DeleteReply {
+            try await nio.deleteAll(where: query).get()
+        }
+        
+        @discardableResult
+        public func deleteAll<Query: MongoKittenQuery>(where query: Query) async throws -> DeleteReply {
+            try await nio.deleteAll(where: query).get()
+        }
+        
+        public func find(_ query: Document = [:]) -> FindQueryBuilder {
+            nio.find(query)
+        }
+        
+        public func find<Query: MongoKittenQuery>(_ query: Query) -> FindQueryBuilder {
+            nio.find(query)
+        }
 
-    public func findOne<D: Decodable>(_ query: Document = [:], as type: D.Type) async throws -> D? {
-        try await collection.findOne(query, as: type).get()
-    }
-    
-    @discardableResult
-    public func insert(_ document: Document) async throws -> InsertReply {
-        try await collection.insert(document).get()
-    }
-    
-    @discardableResult
-    public func insertMany(_ documents: [Document]) async throws -> InsertReply {
-        try await collection.insertMany(documents).get()
-    }
-    
-    @discardableResult
-    public func insertEncoded<E: Encodable>(_ document: E) async throws -> InsertReply {
-        try await collection.insertEncoded(document).get()
-    }
-    
-    @discardableResult
-    public func insertManyEncoded<E: Encodable>(_ documents: [E]) async throws -> InsertReply {
-        try await collection.insertManyEncoded(documents).get()
-    }
-    
-    @discardableResult
-    public func updateOne(
-        where query: Document,
-        to document: Document
-    ) async throws -> UpdateReply {
-        try await collection.updateOne(where: query, to: document).get()
-    }
-    
-    @discardableResult
-    public func updateEncoded<E: Encodable>(
-        where query: Document,
-        to model: E
-    ) async throws -> UpdateReply {
-        try await collection.updateEncoded(where: query, to: model).get()
-    }
-    
-    @discardableResult
-    public func updateOne<Query: MongoKittenQuery>(
-        where query: Query,
-        to document: Document
-    ) async throws -> UpdateReply {
-        try await collection.updateEncoded(where: query, to: document).get()
-    }
-    
-    @discardableResult
-    public func updateEncoded<Query: MongoKittenQuery, E: Encodable>(
-        where query: Query,
-        to model: E
-    ) async throws -> UpdateReply {
-        try await collection.updateEncoded(where: query, to: model).get()
-    }
-    
-    @discardableResult
-    public func updateMany(
-        where query: Document,
-        to document: Document
-    ) async throws -> UpdateReply {
-        try await collection.updateManyEncoded(where: query, to: document).get()
-    }
-    
-    @discardableResult
-    public func updateManyEncoded<E: Encodable>(
-        where query: Document,
-        to model: E
-    ) async throws -> UpdateReply {
-        try await collection.updateManyEncoded(where: query, to: model).get()
-    }
-    
-    @discardableResult
-    public func updateMany<Query: MongoKittenQuery>(
-        where query: Query,
-        to document: Document
-    ) async throws -> UpdateReply {
-        try await collection.updateMany(where: query, to: document).get()
-    }
-    
-    @discardableResult
-    public func updateManyEncoded<Query: MongoKittenQuery, E: Encodable>(
-        where query: Query,
-        to model: E
-    ) async throws -> UpdateReply {
-        try await collection.updateManyEncoded(where: query, to: model).get()
-    }
-    
-    @discardableResult
-    public func updateMany(
-        where query: Document,
-        setting: Document?,
-        unsetting: Document?
-    ) async throws -> UpdateReply {
-        try await collection.updateMany(where: query, setting: setting, unsetting: unsetting).get()
-    }
-    
-    @discardableResult
-    public func upsert(_ document: Document, where query: Document) async throws -> UpdateReply {
-        try await collection.upsert(document, where: query).get()
-    }
-    
-    @discardableResult
-    public func upsertEncoded<E: Encodable>(_ model: E, where query: Document) async throws -> UpdateReply {
-        try await collection.upsertEncoded(model, where: query).get()
-    }
-    
-    @discardableResult
-    public func upsert<Query: MongoKittenQuery>(_ document: Document, where query: Query) async throws -> UpdateReply {
-        try await collection.upsert(document, where: query).get()
-    }
-    
-    @discardableResult
-    public func upsertEncoded<Query: MongoKittenQuery, E: Encodable>(_ model: E, where query: Query) async throws -> UpdateReply {
-        try await collection.upsertEncoded(model, where: query).get()
+        public func find<D: Decodable>(_ query: Document = [:], as type: D.Type) -> MappedCursor<FindQueryBuilder, D> {
+            nio.find(query, as: type)
+        }
+
+        public func findOne<D: Decodable>(_ query: Document = [:], as type: D.Type) async throws -> D? {
+            try await nio.findOne(query, as: type).get()
+        }
+        
+        @discardableResult
+        public func insert(_ document: Document) async throws -> InsertReply {
+            try await nio.insert(document).get()
+        }
+        
+        @discardableResult
+        public func insertMany(_ documents: [Document]) async throws -> InsertReply {
+            try await nio.insertMany(documents).get()
+        }
+        
+        @discardableResult
+        public func insertEncoded<E: Encodable>(_ document: E) async throws -> InsertReply {
+            try await nio.insertEncoded(document).get()
+        }
+        
+        @discardableResult
+        public func insertManyEncoded<E: Encodable>(_ documents: [E]) async throws -> InsertReply {
+            try await nio.insertManyEncoded(documents).get()
+        }
+        
+        @discardableResult
+        public func updateOne(
+            where query: Document,
+            to document: Document
+        ) async throws -> UpdateReply {
+            try await nio.updateOne(where: query, to: document).get()
+        }
+        
+        @discardableResult
+        public func updateEncoded<E: Encodable>(
+            where query: Document,
+            to model: E
+        ) async throws -> UpdateReply {
+            try await nio.updateEncoded(where: query, to: model).get()
+        }
+        
+        @discardableResult
+        public func updateOne<Query: MongoKittenQuery>(
+            where query: Query,
+            to document: Document
+        ) async throws -> UpdateReply {
+            try await nio.updateEncoded(where: query, to: document).get()
+        }
+        
+        @discardableResult
+        public func updateEncoded<Query: MongoKittenQuery, E: Encodable>(
+            where query: Query,
+            to model: E
+        ) async throws -> UpdateReply {
+            try await nio.updateEncoded(where: query, to: model).get()
+        }
+        
+        @discardableResult
+        public func updateMany(
+            where query: Document,
+            to document: Document
+        ) async throws -> UpdateReply {
+            try await nio.updateManyEncoded(where: query, to: document).get()
+        }
+        
+        @discardableResult
+        public func updateManyEncoded<E: Encodable>(
+            where query: Document,
+            to model: E
+        ) async throws -> UpdateReply {
+            try await nio.updateManyEncoded(where: query, to: model).get()
+        }
+        
+        @discardableResult
+        public func updateMany<Query: MongoKittenQuery>(
+            where query: Query,
+            to document: Document
+        ) async throws -> UpdateReply {
+            try await nio.updateMany(where: query, to: document).get()
+        }
+        
+        @discardableResult
+        public func updateManyEncoded<Query: MongoKittenQuery, E: Encodable>(
+            where query: Query,
+            to model: E
+        ) async throws -> UpdateReply {
+            try await nio.updateManyEncoded(where: query, to: model).get()
+        }
+        
+        @discardableResult
+        public func updateMany(
+            where query: Document,
+            setting: Document?,
+            unsetting: Document?
+        ) async throws -> UpdateReply {
+            try await nio.updateMany(where: query, setting: setting, unsetting: unsetting).get()
+        }
+        
+        @discardableResult
+        public func upsert(_ document: Document, where query: Document) async throws -> UpdateReply {
+            try await nio.upsert(document, where: query).get()
+        }
+        
+        @discardableResult
+        public func upsertEncoded<E: Encodable>(_ model: E, where query: Document) async throws -> UpdateReply {
+            try await nio.upsertEncoded(model, where: query).get()
+        }
+        
+        @discardableResult
+        public func upsert<Query: MongoKittenQuery>(_ document: Document, where query: Query) async throws -> UpdateReply {
+            try await nio.upsert(document, where: query).get()
+        }
+        
+        @discardableResult
+        public func upsertEncoded<Query: MongoKittenQuery, E: Encodable>(_ model: E, where query: Query) async throws -> UpdateReply {
+            try await nio.upsertEncoded(model, where: query).get()
+        }
     }
 }
 
