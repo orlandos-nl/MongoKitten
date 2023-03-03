293562357b4b5fe737ebdf10aaf040b9b06f5c1a
Joannis Orlandos <joannis@orlandos.nl>
2023-03-03 14:46:22 +0100

    Make a GetMore command/reply public

--- Sources/MongoClient/InternalCommands/GetMore.swift
+++ Sources/MongoClient/InternalCommands/GetMore.swift
@@ -1,13 +1,13 @@
 import BSON
 
-internal struct GetMore: Encodable, Sendable {
+public struct GetMore: Codable, Sendable {
     /// This variable _must_ be the first encoded value, so keep it above all others
     /// The cursor id
-    internal let getMore: Int64
-    internal let collection: String
-    internal var batchSize: Int?
-    internal var maxTimeMS: Int32?
-    internal var readConcern: ReadConcern?
+    public let getMore: Int64
+    public let collection: String
+    public var batchSize: Int?
+    public var maxTimeMS: Int32?
+    public var readConcern: ReadConcern?
 
     init(cursorId: Int64, batchSize: Int?, collection: String) {
         self.getMore = cursorId
@@ -16,13 +16,13 @@ internal struct GetMore: Encodable, Sendable {
     }
 }
 
-internal struct GetMoreReply: Decodable {
-    struct CursorDetails: Codable {
-        var id: Int64
-        var ns: String
-        var nextBatch: [Document]
+public struct GetMoreReply: Codable {
+    public struct CursorDetails: Codable {
+        public var id: Int64
+        public var ns: String
+        public var nextBatch: [Document]
     }
 
-    internal let cursor: CursorDetails
+    public let cursor: CursorDetails
     private let ok: Int
 }
