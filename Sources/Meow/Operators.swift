960d79d3aaedd9913ba8bf1767e5306d060775ef
Joannis Orlandos <joannis@orlandos.nl>
2023-03-26 14:44:51 +0200

    Disfavor the optional operators

--- Sources/Meow/Operators.swift
+++ Sources/Meow/Operators.swift
@@ -37,32 +37,39 @@ public func == <M: KeyPathQueryableModel, T: RawRepresentable>(lhs: QuerySubject
     return lhs.path.string == rhs.rawValue
 }
 
+@_disfavoredOverload
 public func == <M: KeyPathQueryableModel, T: Primitive>(lhs: QuerySubject<M, T?>, rhs: T?) -> Document {
     return lhs.path.string == rhs
 }
 
+@_disfavoredOverload
 public func == <M: KeyPathQueryableModel, BM>(lhs: QuerySubject<M, Reference<BM>?>, rhs: Reference<BM>?) -> Document where BM.Identifier: Primitive {
     return lhs.path.string == rhs?.reference
 }
 
+@_disfavoredOverload
 public func == <M: KeyPathQueryableModel, BM>(lhs: QuerySubject<M, Reference<BM>?>, rhs: BM.Identifier?) -> Document where BM.Identifier: Primitive {
     return lhs.path.string == rhs
 }
 
+@_disfavoredOverload
 public func == <M: KeyPathQueryableModel, BM>(lhs: QuerySubject<M, Reference<BM>?>, rhs: BM.Identifier?) throws -> Document {
     let rhs = try rhs.encodePrimitive()
     return lhs.path.string == rhs
 }
 
+@_disfavoredOverload
 public func == <M: KeyPathQueryableModel>(lhs: QuerySubject<M, M.Identifier?>, rhs: Reference<M>?) -> Document where M.Identifier: Primitive {
     return lhs.path.string == rhs?.reference
 }
 
+@_disfavoredOverload
 public func == <M: KeyPathQueryableModel>(lhs: QuerySubject<M, M.Identifier?>, rhs: Reference<M>?) throws -> Document {
     let rhs = try rhs?.reference.encodePrimitive()
     return lhs.path.string == rhs
 }
 
+@_disfavoredOverload
 public func == <M: KeyPathQueryableModel, T: RawRepresentable>(lhs: QuerySubject<M, T?>, rhs: T?) -> Document where T.RawValue: Primitive {
     return lhs.path.string == rhs?.rawValue
 }
@@ -81,14 +88,17 @@ public func != <M: KeyPathQueryableModel, T: RawRepresentable>(lhs: QuerySubject
     return lhs.path.string != rhs.rawValue
 }
 
+@_disfavoredOverload
 public func != <M: KeyPathQueryableModel, T: Primitive>(lhs: QuerySubject<M, T?>, rhs: T?) -> Document {
     return lhs.path.string != rhs
 }
 
+@_disfavoredOverload
 public func != <M: KeyPathQueryableModel, BM>(lhs: QuerySubject<M, Reference<BM>?>, rhs: Reference<BM>?) -> Document where BM.Identifier: Primitive {
     return lhs.path.string != rhs?.reference
 }
 
+@_disfavoredOverload
 public func != <M: KeyPathQueryableModel, T: RawRepresentable>(lhs: QuerySubject<M, T?>, rhs: T?) -> Document where T.RawValue: Primitive {
     return lhs.path.string != rhs?.rawValue
 }
