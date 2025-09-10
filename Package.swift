1882e5b3d1ac14e4d71d9ee18dc98d244aed4ed2
Joannis Orlandos <joannis@orlandos.nl>
2025-09-10 17:33:41 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,8 +6,7 @@ import PackageDescription
 let package = Package(
     name: "MongoKitten",
     platforms: [
-        .macOS(.v10_14),
-        .iOS(.v12)
+        .macOS(.v10_15),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
